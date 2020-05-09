0ccd721e082a78bcbc326e89af183aadeec34d47
santi <santi@santi-ZenBook-UX431DA-UM431DA>
2020-05-09 18:41:14 +0200

    cambio de ejercicio 2 al 3 Signed-off-by:santi <santi@santi-ZenBook-UX431DA-UM431DA>

--- JavaApplication5/src/practicagithub1/practicagithub1.java
+++ JavaApplication5/src/practicagithub1/practicagithub1.java
@@ -5,6 +5,9 @@
  */
 package practicagithub1;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author santi
@@ -16,6 +19,28 @@ public class practicagithub1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    double temperaturas[] = new double[7];
+        String diasSemana[]={"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+        double suma = 0;
+        int dia;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        System.out.println("Anota temperatura de la semana");
+        
+        for (int i = 0; i < temperaturas.length; i++){
+            System.out.println(diasSemana[i] + ":");
+            temperaturas[i] = sc.nextDouble();
+            suma = suma + temperaturas[i];
+        }
+        
+        System.out.println("La temperatura media ha sido:" + suma/7);
+        
+        do {
+        System.out.println("Anota dia del que quieras saber la temperatura:");
+        dia = sc.nextInt();
+        }
+        while (dia<1 || dia>7);
+        System.out.println("La temperatura fue: "+temperaturas[dia-1]);
     }
     
 }
